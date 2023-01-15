277e8f22dd21b62088c50aa12b256e7424d25e39
techunity <ezekiyalraja1998@gmail.com>
2023-01-15 02:30:55 -0800

    1

---
+++ Sample git project/New swift file.swift
@@ -0,0 +1,15 @@
+//
+//  New swift file.swift
+//  Sample git project
+//
+//  Created by iOS Developer on 15/01/23.
+//
+
+import Foundation
+class newcontroll
+{
+    var i = 10
+    func FunctionA() {
+        print("New")
+    }
+}
